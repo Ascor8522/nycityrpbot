386cbc0ab4a79e2f84928dd088f2c691766b3027
smrman <vinoa.89.jeux@gmail.com>
2018-01-02 02:05:49 +0100

    ready event added

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+import { Client } from 'discord.js';
+
 const DISCORD = require('discord.js');
 const BOT = new DISCORD.Client();
 
 const FS = require('fs');
 
-FS.readFile('./token', 'utf8', function (err, data) {
+
+FS.readFile('token', 'utf8', function (err, data) {
     if (err) {
-        console.log(err);
+        return console.error(err);
     } else {
-        BOT.login(data);
+        //BOT.login(data);
+        console.log(data);
     }
+    
 });
+
+BOT.on('ready', () => {
+    console.log('Bot pret!');
+    console.log('Connecte en tant que '+ BOT.user.tag);
+});
+
+
+BOT.on('message', message => {
+    let reponse = analyerMessage(message);
+    if (reponse) {
+        message.channel.reponse(reponse);
+    }
+    return;
+});
+
+function analyerMessage() {
+
+}
